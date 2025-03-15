8f90274ffcf9ef08932ee46ba808ae36ac664bcb
Mladentsev_Vyacheslav <vyacheslav.mladentsev@yandex.ru>
2025-03-16 01:59:16 +0700

    добавлены пакеты для организации архитектуры проекта

--- src/main/java/com/mladentsev/taskmanagementsystem/application/TaskManagementSystemApplication.java
+++ src/main/java/com/mladentsev/taskmanagementsystem/application/TaskManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.mladentsev.taskmanagementsystem;
+package com.mladentsev.taskmanagementsystem.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
