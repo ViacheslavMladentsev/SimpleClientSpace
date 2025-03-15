19724657d23bb34e52305719fc1d5442b3d5b95a
Mladentsev_Vyacheslav <vyacheslav.mladentsev@yandex.ru>
2025-03-16 01:35:09 +0700

    Первичная настройка: - добавлен compose.yaml для запуска БД Postgresql в контейнере - добавлен logback.xml с найтройкой записи логов в файл

--- src/main/java/com/mladentsev/taskmanagementsystem/TaskManagementSystemApplication.java
+++ src/main/java/com/mladentsev/taskmanagementsystem/TaskManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.mladentsev.TaskManagementSystem;
+package com.mladentsev.taskmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
